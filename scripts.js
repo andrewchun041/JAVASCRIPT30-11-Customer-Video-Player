cf45d245fe4fd2cbd93dca2f8a3550ba1284f774
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-18 13:01:58 -0700

    add handleProgress function and timeupdate/progress eventListener to change the video progress bar

--- scripts.js
+++ scripts.js
@@ -32,12 +32,19 @@ function handleRangeUpdate() {
     video[this.name] = this.value;
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
 // hook up event listners
 video.addEventListener('click', togglePlay);
-video.addEventListener('play', updateButton);
-video.addEventListener('pause', updateButton)
-
 toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+// video.addEventListener('progress', handleProgress);
+
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
